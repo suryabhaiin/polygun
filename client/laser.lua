6d41828db11fc897e4a726f232e2680daf13c84f
suryabhaiin <54040750+suryabhaiin@users.noreply.github.com>
2024-03-17 10:02:16 +0530

    Create Zone At pointed cooardinate
    Create Zone At pointed cooardinate

--- client/laser.lua
+++ client/laser.lua
@@ -1,3 +1,4 @@
+local RESULT, HEADING, ENTITY, NAME, COORDS, PREVIOUS, ZONE, PREVIOUSZONE, SCALEX, SCALEY, SCALEZ
 local laserEnabled = false
 local laserEndPoint = nil
 lib.hideTextUI()
@@ -38,11 +39,88 @@ local RayCastGamePlayCamera = function(distance)
     local _, b, c, _, e = GetShapeTestResult(StartShapeTestRay(cameraCoord.x, cameraCoord.y, cameraCoord.z, destination.x, destination.y, destination.z, -1, PlayerPedId(), 0))
     return b, c, e
 end
+
+
+local function parseBox(zone)
+	return
+			"{\n"
+			..
+			"\tcoords = " ..
+			"vector3(" ..
+			tostring(roundcord(zone.center.x, 2)) ..
+			", " .. tostring(roundcord(zone.center.y, 2)) .. ", " .. tostring(roundcord(zone.center.z, 2)) .. "),\n"
+			.. "\tlength = " .. tostring(SCALEX) .. ",\n"
+			.. "\twidth = " .. tostring(SCALEY) .. ",\n"
+			.. "\tname = \"" .. zone.name .. "\",\n"
+			.. "\theading = " .. tostring(HEADING) .. ",\n"
+			.. "\tminZ = " .. tostring(roundcord(zone.minZ, 2)) .. ",\n"
+			.. "\tmaxZ = " .. tostring(roundcord(zone.maxZ, 2)) .. ",\n"
+			.. "}\n"
+end
+
+local function destoryZone()
+	if ZONE then ZONE:destroy() end
+end
+
+local function drawEntityZone()
+	destoryZone()
+	ZONE = EntityZone:Create(ENTITY, {
+		name = NAME,
+		debugPoly = true,
+		useZ = true,
+		scale = scale or { SCALEX, SCALEY, SCALEZ }
+	})
+end
+
+function createEntity()
+	local input = lib.inputDialog('Dialog title', {
+		{type = 'input', label = 'Zone Name', description = 'Enter Zone name', required = true, min = 4, max = 16},
+		{type = 'number', label = 'Width X', description = 'Enter width size',  required = true, icon = 'hashtag'},
+		{type = 'number', label = 'Height Y', description = 'Enter height size',  required = true, icon = 'hashtag'},
+		{type = 'number', label = 'Height Z', description = 'Enter height for Z',  required = true, icon = 'hashtag'},
+	  })
+	if not input then return end
+	print(json.encode(input), input[1], input[2])
+	local modelName = "prop_parking_sign_1"
+	NAME = input[1]
+	SCALEX = input[2]
+	SCALEY = input[3]
+	SCALEZ = input[4]
+	if DoesEntityExist(ENTITY) then
+		DeleteEntity(ENTITY)
+	end
+    RequestModel(modelName)
+    while not HasModelLoaded(modelName) do
+        Wait(500)
+    end
+    ENTITY = CreateObject(modelName, laserEndPoint.x, laserEndPoint.y, laserEndPoint.z, true, true, true)
+	SetEntityHeading(ENTITY, HEADING)
+	Wait(100)
+	drawEntityZone()
+end
+
+function saveZone()
+	if ZONE then
+		local text = parseBox(ZONE)
+		if Config.clipboard then lib.setClipboard(text) end
+		TriggerServerEvent('polygun:save', ZONE, text)
+		lib.notify({
+			title = 'Box Zone Saved',
+			duration = 7500,
+			description = 'Check zones.txt inside the polygun resource',
+			type = 'success'
+		})
+		if DoesEntityExist(ENTITY) then
+			DeleteEntity(ENTITY)
+		end
+		destoryZone()
+	end
+end
+
 -- Function to draw the laser effect
 function drawLaser()
 	local playerPed    = PlayerPedId()
-	local player = PlayerId()
-	lib.showTextUI("COPY : [E] vector3 | [Q] vector4 | [F] coords | [R] Json | [H] Heading", {position = 'top-center'})
+	lib.showTextUI("COPY : [E] vector3 | [Q] vector4 | [F] coords | [R] Json | [H] Heading | [G] Add Zone | [X] Save Zone", {position = 'top-center'})
     while laserEnabled do
 		local color = { r = 255, g = 255, b = 255, a = 200 }
 		local position = GetEntityCoords(playerPed)
@@ -52,6 +130,16 @@ function drawLaser()
 		local y = roundcord(coords.y, 2)
 		local z = roundcord(coords.z, 2)
 		local w = roundcord(heading, 2)
+		HEADING = w
+		if IsControlJustReleased(0, 73) then --save zone
+			saveZone()
+			Wait(1000)
+		end
+		if IsControlJustReleased(0, 58) then -- G add zone
+			laserEndPoint = coords
+			createEntity()
+			Wait(1000)
+		end
 		if IsControlJustReleased(0, 51) then -- Copy Vector3 Coords
 			laserEndPoint = string.format('vector3(%s, %s, %s)', x, y, z)
 			lib.setClipboard(laserEndPoint)
@@ -106,6 +194,45 @@ function drawLaser()
 				type = 'success'
 			})
 		end
+		if IsControlJustPressed(0, 241) or IsDisabledControlPressed(1, 241) then -- Scroll Up
+			SCALEX = SCALEX + Config.addX
+			SCALEY = SCALEY + Config.addY
+			SCALEZ = SCALEZ + Config.addZ
+			drawEntityZone()
+		end
+		if IsControlJustPressed(0, 242) or IsDisabledControlPressed(1, 242) then -- Scroll down
+			SCALEX = SCALEX - Config.subX
+			SCALEY = SCALEY - Config.subY
+			SCALEZ = SCALEZ - Config.subZ
+			drawEntityZone()
+		end
+
+		if IsControlPressed(1, Config.addXControl) or IsDisabledControlPressed(1, Config.addXControl) then -- Up Arrow
+			SCALEX = SCALEX + Config.addX
+			drawEntityZone()
+		end
+		if IsControlPressed(1, Config.subXControl) or IsDisabledControlPressed(1, Config.subXControl) then -- Down Arrow
+			SCALEX = SCALEX - Config.subZ
+			drawEntityZone()
+		end
+
+		if IsControlPressed(1, Config.subYControl) or IsDisabledControlPressed(1, Config.subYControl) then -- Left Arrow
+			SCALEY = SCALEY - Config.subY
+			drawEntityZone()
+		end
+		if IsControlPressed(1, Config.addYControl) or IsDisabledControlPressed(1, Config.addYControl) then -- Right Arrow
+			SCALEY = SCALEY + Config.addY
+			drawEntityZone()
+		end
+
+		if IsControlPressed(1, Config.addZControl) or IsDisabledControlPressed(1, Config.addZControl) then -- Page Up
+			SCALEZ = SCALEZ + Config.addZ
+			drawEntityZone()
+		end
+		if IsControlPressed(1, Config.subZControl) or IsDisabledControlPressed(1, Config.subZControl) then -- Page Down
+			SCALEZ = SCALEZ - Config.subZ
+			drawEntityZone()
+		end
 		DrawLine(position.x, position.y, position.z, coords.x, coords.y, coords.z, color.r, color.g, color.b, color.a)
         DrawMarker(28, coords.x, coords.y, coords.z, 0.0, 0.0, 0.0, 0.0, 180.0, 0.0, 0.1, 0.1, 0.1, color.r, color.g, color.b, color.a, false, true, 2, nil, nil, false, false)
 		Wait(10)
@@ -117,6 +244,13 @@ RegisterNetEvent('polygun:runlaser', function()
 	laserEnabled = not laserEnabled
     if not laserEnabled then
 		laserEndPoint = nil
+		if DoesEntityExist(ENTITY) then
+			DeleteEntity(ENTITY)
+		end
+		destoryZone()
+		-- If the vehicle does exist, delete the vehicle entity from the game world.
+
+
 	else
 		drawLaser()
 	end
